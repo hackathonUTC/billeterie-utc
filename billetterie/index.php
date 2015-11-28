5718af7d3610e3016f89b5dbe28aab5f75d60ba4
unknown <jdekhtia@GSMPORT11.utc>
2015-11-28 22:22:06 +0100

    Billetterie Cleaned

--- billetterie/index.php
+++ billetterie/index.php
@@ -41,55 +41,56 @@
         
         <div class="container">
 		
-		<div id="wrap">
-                
-			<div class="page-header">
-				<a href="#" id="logout" class="btn btn-danger pull-right" style="display: none;"> Logout </a>
-				<h1><center>Liste des événements</center></h1>
-				
-				
-			</div>
-		
-			<div class="row">
-			<table class="table">
-				<tbody>
-				  <tr class="success">
-					<td><div class="span3"><img src="image/affiche.jpg" alt="affiche-evenement style="width=200px;height=200px"></div></td>
-					<td><div class="span6"><br><h3><center>Estu Noel <br> <br> nombre de place restantes : 2 </center></h3></div></td>
-					<td><div class="span3"><br><br><center><br><br><a href="billetterie.php" class="btn btn-primary" role="button">Acheter des places</a></center></div></td>
-				  </tr>
-				  <tr class="warning">
-					<td><div class="span3"><img src="image/affiche2.jpg" alt="affiche-evenement style="width=200px;height=200px"></div></td>
-					<td><div class="span6"><br><h3><center>Hackathon<br> <br> nombre de place restantes : 0 </center></h3></div></td>
-					<td><div class="span3"><br><br><center><br><br><a href="billetterie.php" class="btn btn-primary disabled" role="button">Acheter des places</a></td>
-				  </tr>
-				  <tr class="info">
-					<td><div class="span3"><img src="image/affiche3.jpg" alt="affiche-evenement style="width=200px;height=200px"></div></td>
-					<td><div class="span6"><br><h3><center>Ski UTC<br> <br> nombre de place restantes : 73 </center></h3></div></td>
-					<td><div class="span3"><br><br><center><br><br><a href="billetterie.php" class="btn btn-primary" role="button">Acheter des places</a></td>
-				  </tr>
-				</tbody>
-			  </table>
+			<div id="wrap">
+					
+				<div class="page-header">
+					<a href="#" id="logout" class="btn btn-danger pull-right" style="display: none;"> Logout </a>
+					<h1><center>Liste des événements</center></h1>
+					
+					
 				</div>
+			
+				<div class="row">
+					<table class="table">
+						<tbody>
+						  <tr class="success">
+							<td><div class="span3"><img src="image/affiche.jpg" alt="affiche-evenement style="width=200px;height=200px"></div></td>
+							<td><div class="span6"><br><h3><center>Estu Noel <br> <br> nombre de place restantes : 2 </center></h3></div></td>
+							<td><div class="span3"><br><br><center><br><br><a href="billetterie.php" class="btn btn-primary" role="button">Acheter des places</a></center></div></td>
+						  </tr>
+						  <tr class="warning">
+							<td><div class="span3"><img src="image/affiche2.jpg" alt="affiche-evenement style="width=200px;height=200px"></div></td>
+							<td><div class="span6"><br><h3><center>Hackathon<br> <br> nombre de place restantes : 0 </center></h3></div></td>
+							<td><div class="span3"><br><br><center><br><br><a href="billetterie.php" class="btn btn-primary disabled" role="button">Acheter des places</a></td>
+						  </tr>
+						  <tr class="info">
+							<td><div class="span3"><img src="image/affiche3.jpg" alt="affiche-evenement style="width=200px;height=200px"></div></td>
+							<td><div class="span6"><br><h3><center>Ski UTC<br> <br> nombre de place restantes : 73 </center></h3></div></td>
+							<td><div class="span3"><br><br><center><br><br><a href="billetterie.php" class="btn btn-primary" role="button">Acheter des places</a></td>
+						  </tr>
+						</tbody>
+					  </table>
+						</div>
+					</div>
+				  
+				</div>
+				
+				<?php
+					include("../parts/footer.php");
+				?>
+			 
+				<div class="modal hide fade" id="modal" style="display: none;">
+					<div class="modal-header">
+						<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+						<h3 id="modal-header"></h3>
+					</div>
+					<div class="modal-body" id="modal-body">
+					</div>
+				</div>
+
+
 			</div>
-          
-        </div>
-            
-            <?php
-                include("../parts/footer.php");
-            ?>
-         
-            <div class="modal hide fade" id="modal" style="display: none;">
-                <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                    <h3 id="modal-header"></h3>
-                </div>
-                <div class="modal-body" id="modal-body">
-                </div>
-            </div>
-
-
-        </div>
+		</div>
     
         <script src="../bootstrap/js/bootstrap-modal.js"></script>
         <script src="../bootstrap/js/bootstrap.min.js"></script>
