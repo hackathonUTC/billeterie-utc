e05d08a9fb6a4d68d125de639f5c7644aeb3ad8a
unknown <jdekhtia@GSMPORT11.utc>
2015-11-28 23:46:22 +0100

    Vars changed from GET to POST

--- admin/connect.php
+++ admin/connect.php
@@ -4,8 +4,8 @@
 	$root = realpath($_SERVER["DOCUMENT_ROOT"]);
 	require_once $root.'/config.inc.php';
 
-	$email = isset($_GET['email']) ? $_GET['email'] : '';
-	$password = isset($_GET['email']) ? $_GET['email'] : '';
+	$email = isset($_POST['email']) ? $_POST['email'] : '';
+	$password = isset($_POST['email']) ? $_POST['email'] : '';
 	
 	if ($email == "" or $password == "")
 		//header('Location: '.$_CONFIG['home']);
