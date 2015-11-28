39615f31e0ba78d972bf8fec65adade4a88c2bbe
Amaury Fischer <amaufischer@gmail.com>
2015-11-29 00:17:28 +0100

    Menu Added

---
+++ admin/parts/menu.php
@@ -0,0 +1,17 @@
+﻿<aside id="sidebar" class="column">
+	<form class="quick_search">
+		<input type="text" value="Quick Search" onfocus="if(!this._haschanged){this.value=''};this._haschanged=true;">
+	</form>
+	<hr/>
+	<h3>Événements</h3>
+	<ul class="toggle">
+		<li class="icn_new_article"><a href="create-event.php">Créer un événement</a></li>
+		<li class="icn_edit_article"><a href="#">Gestion des événement</a></li>
+	</ul>
+	<h3>Administration</h3>
+	<ul class="toggle">
+		<li class="icn_add_user"><a href="index.php">Panneau d'administration</a></li>
+		<li class="icn_add_user"><a href="gestion-asso.php">Gestion de l'association</a></li>
+	</ul>
+	
+</aside><!-- end of sidebar -->
