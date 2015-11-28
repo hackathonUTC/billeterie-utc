71bb462c74d47323f1cce44bb68fb2cf51feca4e
unknown <jdekhtia@GSMPORT11.utc>
2015-11-28 23:50:06 +0100

    Config File Changed

--- config.inc.dist.php
+++ config.inc.dist.php
@@ -4,7 +4,8 @@
 $_CONFIG['sql_host'] = "localhost";
 $_CONFIG['sql_db'] = "billetterie_utc";
 $_CONFIG['sql_user'] = "root";
-$_CONFIG['sql_pass'] = "root";
+$_CONFIG['sql_pass'] = "";
+$_CONFIG['sql_port'] = 3306;
 
 // Chemin vers le serveur CAS (avec le / final)
 $_CONFIG['cas_url'] = "https://cas.utc.fr/cas/";
