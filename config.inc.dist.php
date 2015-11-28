5ef7bca199cf8e023df01afc43be633312775906
unknown <jdekhtia@GSMPORT11.utc>
2015-11-28 19:42:47 +0100

    Config File Modified

--- config.inc.dist.php
+++ config.inc.dist.php
@@ -1,13 +1,14 @@
 <?php
 
 // Paramètres de BDD
-$_CONFIG['sql_host'] = "################";
-$_CONFIG['sql_db'] = "################";
-$_CONFIG['sql_user'] = "################";
-$_CONFIG['sql_pass'] = "################";
+$_CONFIG['sql_host'] = "localhost";
+$_CONFIG['sql_db'] = "billetterie_utc";
+$_CONFIG['sql_user'] = "root";
+$_CONFIG['sql_pass'] = "root";
 
 // Chemin vers le serveur CAS (avec le / final)
-$_CONFIG['cas_url'] = "https://cas.utc.fr/cas/login";
-$_CONFIG['service'] = "http://localhost/";
+$_CONFIG['cas_url'] = "https://cas.utc.fr/cas/";
+$_CONFIG['home'] = "http://localhost/";
+$_CONFIG['service'] = $_CONFIG['home']."inc/connect.php";
 
 ?>
