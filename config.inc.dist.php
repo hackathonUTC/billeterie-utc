52fb87d627712effe07bfa802f054ef488df32a4
unknown <jdekhtia@GSMPORT11.utc>
2015-11-28 17:08:00 +0100

    Config File added

---
+++ config.inc.dist.php
@@ -0,0 +1,13 @@
+<?php
+
+// Paramètres de BDD
+$_CONFIG['sql_host'] = "################";
+$_CONFIG['sql_db'] = "################";
+$_CONFIG['sql_user'] = "################";
+$_CONFIG['sql_pass'] = "################";
+
+// Chemin vers le serveur CAS (avec le / final)
+$_CONFIG['cas_url'] = "https://cas.utc.fr/cas/login";
+$_CONFIG['service'] = "http://localhost/";
+
+?>
