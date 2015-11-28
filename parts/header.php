6a0ed73945b8e28ccb3c8960cce65d813ebd0f99
unknown <jdekhtia@GSMPORT11.utc>
2015-11-28 21:16:20 +0100

    Header Corrected

--- parts/header.php
+++ parts/header.php
@@ -1,7 +1,14 @@
+<?php 
+	if (substr_count($_SERVER["REQUEST_URI"], '/') == 1)
+		$pre_url = "/";
+	else
+		$pre_url = "../";
+?>
+
 <div class="navbar navbar-inverse navbar-fixed-top" >
     <div class="navbar-inner">
-    <a href="https://github.com/JonathanDekhtiar/billetterieUTC">
-    <img style="position: absolute; top: 0; right: 0; border: 0;" src="/billetterie/images/forkme.png">
+    <a href="https://github.com/hackathonUTC/billetterie-utc">
+    <img style="position: absolute; top: 0; right: 0; border: 0;" src="<?php echo $pre_url; ?>images/forkme.png">
     </a>
     
         <div class="container" style="width:1170px;">
@@ -10,26 +17,26 @@
             <span class="icon-bar"></span>
 
             </a>
-            <a class="brand" href="/">Billetterie Évènementielle UTC</a>
+            <a class="brand" href="<?php echo $pre_url; ?>">Billetterie Évènementielle UTC</a>
             <div class="nav-collapse collapse">
                 <ul class="nav">
                     <li id="home">
-                        <a href="/">Home</a>
+                        <a href="<?php echo $pre_url; ?>">Home</a>
                     </li>
 					<li id="billetterie">
-                        <a href="/billetterie/">Billetterie</a>
+                        <a href="<?php echo $pre_url; ?>billetterie/">Billetterie</a>
                     </li>
-                    <li id="/setup/">
-                        <a href="setup/">Setting Up NFC</a>
+                    <li id="setup">
+                        <a href="<?php echo $pre_url; ?>setup/">Setting Up NFC</a>
                     </li>
-                    <li id="/offline/">
-                        <a href="offline/">Setting Up Offline Server</a>
+                    <li id="offline">
+                        <a href="<?php echo $pre_url; ?>offline/">Setting Up Offline Server</a>
                     </li>
-                    <li id="/contact/">
-                        <a href="contact/">Contact</a>
+                    <li id="contact">
+                        <a href="<?php echo $pre_url; ?>contact/">Contact</a>
                     </li>
-                    <li id="/admin/">
-                        <a href="admin/">Administration</a>
+                    <li id="admin">
+                        <a href="<?php echo $pre_url; ?>admin/">Administration</a>
                     </li>
                     
                 </ul>
