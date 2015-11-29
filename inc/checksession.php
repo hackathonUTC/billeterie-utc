0d4463ca75a173b40ee2a6a0a79f29dbc3a4cd33
unknown <jdekhtia@GSMPORT11.utc>
2015-11-29 10:19:05 +0100

    checksession cleaned

--- inc/checksession.php
+++ inc/checksession.php
@@ -1,11 +1,11 @@
 <?php
-	// Page d'accueil : /index.php 
-	header("Content-Type: text/html; charset=UTF-8"); 
+	// Page d'accueil : /index.php
+	header("Content-Type: text/html; charset=UTF-8");
 	$root = realpath($_SERVER["DOCUMENT_ROOT"]);
 
  	session_start();
 	if( !isset($_SESSION['login']) || $_SESSION['login'] == '' ) {
-    	
+
     	$currentAddr = "http://".$_SERVER["HTTP_HOST"].$_SERVER["REQUEST_URI"];
     	if ($currentAddr != $_CONFIG['home'])
     		header('Location: '.$_CONFIG['home']);
