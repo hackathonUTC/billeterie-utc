a4f102978bc92713ee610949558247ead6105858
unknown <jdekhtia@GSMPORT11.utc>
2015-11-29 10:19:12 +0100

    dbconnect show errors

--- inc/dbconnect.php
+++ inc/dbconnect.php
@@ -1,11 +1,12 @@
 <?php
     $root = realpath($_SERVER["DOCUMENT_ROOT"]);
     require_once $root.'/config.inc.php';
-    
+
     try {
-        $connexion = new PDO('mysql:host='.$_CONFIG['sql_host'].';port='.$_CONFIG['sql_port'].';dbname='.$_CONFIG['sql_db'], $_CONFIG['sql_user'], $_CONFIG['sql_pass'],  array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));   
-        
-    }catch ( Exception $e ) 
+        $connexion = new PDO('mysql:host='.$_CONFIG['sql_host'].';port='.$_CONFIG['sql_port'].';dbname='.$_CONFIG['sql_db'], $_CONFIG['sql_user'], $_CONFIG['sql_pass'],  array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
+        $connexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    }catch ( Exception $e )
     {
         echo "Connection à MySQL impossible : <br>", $e->getMessage();
         echo '<meta http-equiv="Refresh" content="5; Url='.$_CONFIG['home'].'">';
